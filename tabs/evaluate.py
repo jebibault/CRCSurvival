970b8d62a84836b5aa2a7b1fc6f4548e85da5100
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 10:54:46 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -4,24 +4,23 @@ import dash_html_components as html
 
 from app import app
 
-layout = html.Div([
-	dcc.Markdown("""
-		### Metrics
-		Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
-		of the model. All metrics are reported on the test dataset.
+layout = [dcc.Markdown("""
+### Metrics
 
-		|  Metric      |  Result   |
-		|--------------|-----------|
-		| Accuracy     |  91.9%    |
-		| Precision    | 80.78%    |
-		| Recall       | 77.5%     |
-		| ROC AUC      | 92.04%    |
-		| PR AUC       | 84.88%    |
+Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
+of the model. All metrics are reported on the test dataset.
 
-	"""),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
-	dcc.Markdown("""ROC AUC:"""),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
-	dcc.Markdown("""Precision Recall AUC:"""),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')],
-	style={'text-align':'center'})
+|  Metric      |  Result   |
+|--------------|-----------|
+| Accuracy     |  91.9%    |
+| Precision    | 80.78%    |
+| Recall       | 77.5%     |
+| ROC AUC      | 92.04%    |
+| PR AUC       | 84.88%    |
+
+"""),
+
+html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
+html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
+html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')
+         ]
