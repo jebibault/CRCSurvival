c941d6bba4d93aeb1c1ccbb3619cb690995dd326
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 15:24:52 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -5,9 +5,38 @@ import dash_html_components as html
 from app import app
 
 layout = [
-	dcc.Markdown("""#### Confusion matrix:"""),
-	html.Div([
-		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')],
-		style={'text-align':'center'}
-	)
+dcc.Markdown("""
+		### Metrics
+		Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
+		of the model. All metrics are reported on the test dataset.
+
+		|  Metric      |  Result   |
+		|--------------|-----------|
+		| Accuracy     |  91.9%    |
+		| Precision    | 80.78%    |
+		| Recall       | 77.5%     |
+		| ROC AUC      | 92.04%    |
+		| PR AUC       | 84.88%    |
+
+	"""),
+dcc.Markdown("""#### Confusion matrix:"""),
+html.Div([
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')],
+	style={'text-align':'center'}),
+dcc.Markdown("""#### ROC AUC:"""),
+html.Div([
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true')],
+	style={'text-align':'center'}),
+dcc.Markdown("""#### Precision Recall AUC:"""),
+html.Div([
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true')],
+	style={'text-align':'center'}),
+dcc.Markdown("""#### Kaplan-Meier survival curve:"""),
+html.Div([
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')],
+	style={'text-align':'center'}),
+dcc.Markdown("""#### The 20 most important features predicting survival (according to SHAP values:)"""),
+html.Div([
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true')],
+	style={'text-align':'center'}),
 ]
