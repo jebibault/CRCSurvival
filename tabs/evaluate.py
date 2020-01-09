0ad832b4cd875b33abc2f3f3c77ee1e83e8ab74c
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 10:23:38 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -4,25 +4,33 @@ import dash_html_components as html
 
 from app import app
 
-layout = [dcc.Markdown("""
-### Metrics
+layout = html.Div([
+	dcc.Markdown("""
+		### Metrics
+		Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
+		of the model. All metrics are reported on the test dataset.
 
-Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
-of the model. All metrics are reported on the test dataset.
+		|  Metric      |  Result   |
+		|--------------|-----------|
+		| Accuracy     |  91.9%    |
+		| Precision    | 80.78%    |
+		| Recall       | 77.5%     |
+		| ROC AUC      | 92.04%    |
+		| PR AUC       | 84.88%    |
 
-Accuracy: 91.19%
-f1 Score: 79.10%
-Average Precision: 84.88%
-Precision: 80.78%
-Negative Predictive Value: 91.05%
-Recall: 77.50%
-False Positive Rate: 8.95%
-Specificity: 91.05%
-False Negative Rate: 16.97%
-False Discovery Rate: 16.97%
-AUC: 92.04%
-"""),
-
-html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
-html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
-html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')]
+	"""),
+	html.Img(
+		src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true',
+	),
+	dcc.Markdown("""
+		ROC AUC:
+		"""),
+	html.Img(
+		src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'
+	),
+	dcc.Markdown("""
+		Precision Recall AUC:
+		"""),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true'
+	)
+], style={'text-align':'center'})
