7742fe1bc69383978423e3f795a930d23f02d64d
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-08 20:49:04 -0800

    Update explain.py

--- tabs/explain.py
+++ tabs/explain.py
@@ -7,13 +7,11 @@ from app import app
 layout = [dcc.Markdown("""
 #### The PLCO Trial
 The effect of screening with prostate-specific–antigen (PSA) testing and digital rectal examination on the rate of death
-from prostate cancer is unknown. This is the first report from the Prostate, Lung, Colorectal, and Ovarian (PLCO) 
-Cancer Screening Trial on prostate-cancer mortality. From 1993 through 2001, we randomly assigned 76,693 men at 10 
-U.S. study cen- ters to receive either annual screening (38,343 subjects) or usual care as the control (38,350 subjects). 
+from prostate cancer is unknown. The Prostate, Lung, Colorectal, and Ovarian (PLCO) Cancer Screening Trial was conducted
+to assess the role of prostate cancer screening on survival. From 1993 through 2001, 76,693 men at 10 
+U.S. study centers were randomized to receive either annual screening (38,343 subjects) or usual care as the control (38,350 subjects). 
 Men in the screening group were offered annual PSA testing for 6 years and digital rectal examination for 4 years. 
 The subjects and health care providers received the results and decided on the type of follow-up evaluation. 
-Usual care sometimes included screening, as some organizations have recommend- ed. The numbers of all cancers 
-and deaths and causes of death were ascertained.
 
 Results of the trial were published in three articles:
 - https://www.ncbi.nlm.nih.gov/pubmed/19297565
@@ -21,7 +19,7 @@ Results of the trial were published in three articles:
 - https://www.ncbi.nlm.nih.gov/pubmed/27911486
 
 #### Data
-The data with 13 years median follow-up was obtained from the National Cancer Institute Cancer Data Access System:
+The data with a 13-years median follow-up was obtained from the National Cancer Institute Cancer Data Access System:
 https://cdas.cancer.gov/plco/
 
 #### Model Creation
