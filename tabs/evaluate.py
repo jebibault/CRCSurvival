cc6d6c2bb397669327eaa8c288684a2d4eb2b016
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 15:09:37 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -4,15 +4,10 @@ import dash_html_components as html
 
 from app import app
 
-layout = html.Div([
-	html.P('Confusion matrix:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'ROC AUC:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'Precision Recall AUC:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'Kaplan-Meier overall survival curves:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'The 20 most important features predicting survival (according to SHAP values:)'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true')
-], style={'text-align':'center'})
+layout = [
+	dcc.Markdown("""#### Confusion matrix:"""),
+	html.Div([
+		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')],
+		style={'text-align':'center'}
+	)
+]
