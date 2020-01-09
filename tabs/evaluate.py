70910fea4d7a3b37252c9a1ecc8155e84fcf21e8
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 14:49:56 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -4,39 +4,15 @@ import dash_html_components as html
 
 from app import app
 
-layout = html.Div(
-	html.P([
-		html.Br(),
-		'Confusion matrix:', 
-		html.Br()
-	]),
+layout = html.Div([
+	html.P('Confusion matrix:'),
 	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
-	html.P([
-		html.Br(),
-		html.Br(),
-		'ROC AUC:',
-		html.Br()
-	]),
-		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
-	html.P([
-		html.Br(),
-		html.Br(),
-		'Precision Recall AUC:',
-		html.Br(),
-	]),
-		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPR.png?raw=true'),
-	html.P([
-		html.Br(),
-		html.Br(),
-		'Kaplan-Meier overall survival curves:',
-		html.Br(),
-	]),
-		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true'),
-	html.P([
-		html.Br(),
-		html.Br(),
-		'The 20 most important features predicting survival (according to SHAP values:)',
-		html.Br(),
-	]),
-		html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yTreeExplainer.png?raw=true')
-)
+	html.P(html.Br(), html.Br(), 'ROC AUC:'),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
+	html.P(html.Br(), html.Br(), 'Precision Recall AUC:'),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPR.png?raw=true'),
+	html.P(html.Br(), html.Br(), 'Kaplan-Meier overall survival curves:'),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true'),
+	html.P(html.Br(), html.Br(), 'The 20 most important features predicting survival (according to SHAP values:)'),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yTreeExplainer.png?raw=true')
+])
