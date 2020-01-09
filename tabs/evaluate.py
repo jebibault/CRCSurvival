bab4cb279aa13ccc4da44b20527607efafcab1b9
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 14:57:09 -0800

    Update evaluate.py

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -4,15 +4,25 @@ import dash_html_components as html
 
 from app import app
 
-layout = html.Div([
-	html.P('Confusion matrix:'),
+layout = 
+	dcc.Markdown("""
+		### Metrics
+		Evaluating models trained on unbalanced classes can be difficult. We provide several methods to assess the performances 
+		of the model. All metrics are reported on the test dataset.
+
+		|  Metric      |  Result   |
+		|--------------|-----------|
+		| Accuracy     |  91.9%    |
+		| Precision    | 80.78%    |
+		| Recall       | 77.5%     |
+		| ROC AUC      | 92.04%    |
+		| PR AUC       | 84.88%    |
+
+	"""),
+	html.Div([
 	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'ROC AUC:'),
+	dcc.Markdown("""ROC AUC:"""),
 	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'Precision Recall AUC:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPR.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'Kaplan-Meier overall survival curves:'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true'),
-	html.P(html.Br(), html.Br(), 'The 20 most important features predicting survival (according to SHAP values:)'),
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yTreeExplainer.png?raw=true')
-])
+	dcc.Markdown("""Precision Recall AUC:"""),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')]
+	style={'text-align':'center'})
