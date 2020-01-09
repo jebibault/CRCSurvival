8cf6bf51c0de9ca80d8c856692607fb9b0aa8b95
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-08 17:46:21 -0800

    Update intro.py

--- tabs/intro.py
+++ tabs/intro.py
@@ -6,18 +6,11 @@ from app import app
 
 layout = [dcc.Markdown("""
 ### Intro
-Marin County California is one of the most expensive residential real estate markets in the country.  It is also
-one of the most competitive markets with more than 45% of all single-family homes in 2018
-receiving multiple offers.  How does a buyer or buyer's agent determine the optimal price to bid on a home -
-a price high enough to win the bidding war, yet not too high over the next highest bid.  Essentially it is
-a classic auction problem.
+This model was trained on data from the prospective randomized multicenter trial PLCO that assessed the interest of prostate cancer screening with annual PSA testing for 6 years and digital rectal examination for 4 years.
 
-This web app enables the user to determine the predicted price to pay for a home facing a bidding a war.
-The predicted price is based on historical data from 2015 - 2019 for all single-family homes sold in Marin
-receiving two or more offers.  The user can select the area, number of bedrooms, number of baths, number of
-expected offers and listing price and the app will provide the predicted sales price.
+From 1993 through 2001, 76,693 men at 10 U.S. study centers were randomly assigned to receive either annual screening (38,343 subjects) or usual care as the control (38,350 subjects).
 
-As a rule of thumb, real estate agents have used anywhere from 2 to 3 percent per offer to determine the
-price to pay in a bidding war.  For example, if there are 3 offers the bid should be anywhere from 6% to
-9% over the list price.
+To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=13,738). The dataset was split into a training (n=10,990) and a testing (n=2748) dataset.
+
+XGBoost was used to predict 10-year overall survival. Hyperparameters were tuned with Bayesian Optimization in a nested cross-validation manner. Performances were evaluated on the test dataset.
 """)]
