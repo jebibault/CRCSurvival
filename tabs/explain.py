110d6446ef8cc1aa662c37eb496b73ee3e1c5803
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 22:37:52 -0800

    Update explain.py

--- tabs/explain.py
+++ tabs/explain.py
@@ -16,20 +16,20 @@ layout = [
 	
 	dcc.Link('Andriole GL et al., New England Journal of Medicine, 2009', 
 		 href='https://www.ncbi.nlm.nih.gov/pubmed/19297565',
-		 target="_blank",
+		 target='_blank',
 		 style={'font-weight': 'bold', 'color': '#820000'}),
 	
 	html.P([]),
 	
 	dcc.Link('Andriole GL et al., Journal of the National Cancer Institute, 2012', 
 		 href='https://www.ncbi.nlm.nih.gov/pubmed/22228146',
-		 target="_blank",
+		 target='_blank',
 		 style={'font-weight': 'bold', 'color': '#820000'}),
 	
 	html.P([]),
 	
 	dcc.Link('Pinsky PF et al., Cancer, 2017', href='https://www.ncbi.nlm.nih.gov/pubmed/19297565',
-		 target="_blank",
+		 target='_blank',
 		 style={'font-weight': 'bold', 'color': '#820000'}),
 	
 	html.P([]),
