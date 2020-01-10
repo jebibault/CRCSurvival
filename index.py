695342a570e9c1f066fb76986203a03f21490af3
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 20:21:51 -0800

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from app import app, server
 from tabs import intro, predict, explain, evaluate
 
 style = {
-    #'maxWidth': '960px', 
+    'maxWidth': '800px', 
     'margin': 'auto'}
 
 app.title = 'Prostate cancer survival prediction with AI'
@@ -18,8 +18,14 @@ app.layout = html.Div([
                  "margin-bottom": "25px"
              }
             ),
-    dcc.Markdown("# Predict prostate cancer survival with AI"),
-    dcc.Markdown("""This model allows you to predict 10-year overall survival in patients with prostate cancer."""),
+    dcc.Markdown("## Predict prostate cancer survival with AI"),
+    html.P([
+		'This model allows you to predict 10-year overall survival in patients with prostate cancer.', 
+		html.Br(),
+		html.Br(),
+		html.Br()
+	])
+    #dcc.Markdown("""This model allows you to predict 10-year overall survival in patients with prostate cancer."""),
     dcc.Tabs(id='tabs', value='tab-intro', parent_className='custom-tabs', className='custom-tabs-container', children=[
         dcc.Tab(label='Introduction', value='tab-intro', className='custom-tab', selected_className='custom-tab--selected'),
         dcc.Tab(label='Methods', value='tab-explain', className='custom-tab', selected_className='custom-tab--selected'),
