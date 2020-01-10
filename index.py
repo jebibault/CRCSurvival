cd917048c0563c7bad59ae308169f0677bb7234c
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-09 16:53:07 -0800

    Update index.py

--- index.py
+++ index.py
@@ -9,6 +9,23 @@ style = {'maxWidth': '960px', 'margin': 'auto'}
 
 app.title = 'Prostate cancer survival prediction with AI'
 
+tabs_styles = {
+    'height': '44px'
+}
+tab_style = {
+    'borderBottom': '1px solid #d6d6d6',
+    'padding': '6px',
+    'fontWeight': 'bold'
+}
+
+tab_selected_style = {
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'backgroundColor': '#119DFF',
+    'color': 'white',
+    'padding': '6px'
+}
+
 app.layout = html.Div([
     html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/logo.png?raw=true',
              style={
@@ -19,15 +36,15 @@ app.layout = html.Div([
     dcc.Markdown("# Predict prostate cancer survival with AI"),
     dcc.Markdown("""This model allows you to predict 10-year overall survival in patients with prostate cancer."""),
     dcc.Tabs(id='tabs', value='tab-intro', children=[
-        dcc.Tab(label='Introduction', value='tab-intro'),
-        dcc.Tab(label='Methods', value='tab-explain'),
-        dcc.Tab(label='Performances', value='tab-evaluate'),
-        dcc.Tab(label='Predict', value='tab-predict')
-    ], colors={
-        "border": "maroon"
-    }),
-    html.Div(id='tabs-content'),
-], style=style)
+        dcc.Tab(label='Introduction', value='tab-intro', style='tab_style', selected_style='tab_selected_style'),
+        dcc.Tab(label='Methods', value='tab-explain', style='tab_style', selected_style='tab_selected_style'),
+        dcc.Tab(label='Performances', value='tab-evaluate', style='tab_style', selected_style='tab_selected_style'),
+        dcc.Tab(label='Predict', value='tab-predict', style='tab_style', selected_style='tab_selected_style')
+    ]),
+    html.Div(id='tabs-content-inline'),
+], 
+    #style=style
+)
 
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs', 'value')])
