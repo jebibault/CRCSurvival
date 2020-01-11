a0edbb3542bcfb392f6dc66247ca7ac17a13255d
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-10 18:01:14 -0800

    Add files via upload

--- tabs/intro.py
+++ tabs/intro.py
@@ -19,7 +19,7 @@ layout = html.Div(
 		html.Br(),
 		html.Br(),
 		html.Br(),
-		'To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=13,738).'
+		'To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=13,738).',
 		html.Br(),
 		html.Br(),
 		html.Br(),
@@ -28,5 +28,3 @@ layout = html.Div(
 		'Disclaimer: All information contained on this application is for general informational purposes only and does not constitute medical or other professional advice. It should not be used for medical purpose. The information you find on this application is provided as is and to the extent permitted by law, the creators disclaim all other warranties of any kind, whether express, implied, statutory or otherwise. In no event will the authors or its affiliates be liable for any damages whatsoever, including without limitation, direct, indirect, incidental, special, exemplary, punitive or consequential damages, arising out of the use, inability to use or the results of use of this application, any websites linked to this application or the materials or information or services contained at any or all such websites, whether based on warranty, contract, tort or any other legal theory and whether or not advised of the possibility of such damages. If your use of the materials, information or services from this application or any website linked to this application results in the need for servicing, repair or correction of equipment or data, you assume all costs thereof.'
 	])
 )
-
-
