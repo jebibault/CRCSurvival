f7f887781bf59fc79a6cc47a85bd6d10a875e3ac
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-10 18:25:16 -0800

    Add files via upload

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -23,27 +23,27 @@ dcc.Markdown("""
 	This is a table with two rows and two columns that reports the number of false positives, false negatives, true positives, and true negatives.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')] style={"text-align":"center", 'width' : '80%'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### ROC AUC
 	The ROC curve is created by plotting the true positive rate (y-axis) and the false positive rate (x-axis) at various threshold settings.
 	It illustrates the diagnostic ability of the model as its discrimination threshold is varied. The optimal value of the AUC a model is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true')] style={"text-align":"center", 'width' : '80%'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### Precision Recall AUC
 	A precision-recall curve is a plot of the precision (y-axis) and the recall (x-axis) for different probability thresholds.
 	The optimal value of the PR AUC is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true')] style={"text-align":"center", 'width' : '80%'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### Kaplan-Meier survival curve
 	This curve estimates the survival of the patients included in the study. High-risk patients are patients that the model identifies as having a high risk of dying within 10 years from diagnosis.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')] style={"text-align":"center", 'width' : '80%'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### The 20 most important features predicting survival
 	The SHAP value is a way to estimate the contribution of a feature to the prediction performed by the model.
@@ -53,5 +53,5 @@ dcc.Markdown("""
 	- Correlation: Younger patients (blue dots) at diagnosis have a lower risk of dyng within 10 years (negative SHAP values).
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true')] style={"text-align":"center", 'width' : '80%'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true')], style={'text-align' : 'center'})
 ]
