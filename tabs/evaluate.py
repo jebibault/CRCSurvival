bd0bc6a3f7a5e32bc56a78f9356a5790ebd22511
Jean-Emmanuel Bibault <jebibault@gmail.com>
2020-01-10 16:52:09 -0800

    Add files via upload

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -11,11 +11,11 @@ dcc.Markdown("""
 
 	|  Metric      |  Result   |  Definition					                   |
 	|--------------|-----------|-----------------------------------------------------------------------|
-	| Accuracy     | 91.90%    |  Number of correct predictions / total number of input samples        |
-	| Precision    | 80.78%    |  Number of correct positive predictions / number of positive samples  |
-	| Recall       | 77.50%    |  Number of correct positive results / number of all positive samples  |
-	| ROC AUC      | 92.04%    |  See below                                                            |
-	| PR AUC       | 84.88%    |  See below                                                            |
+	| Accuracy     | 91.34%    |  Number of correct predictions / total number of input samples        |
+	| Precision    | 80.70%    |  Number of correct positive predictions / number of positive samples  |
+	| Recall       | 78.51%    |  Number of correct positive results / number of all positive samples  |
+	| ROC AUC      | 93.04%    |  See below                                                            |
+	| PR AUC       | 86.83%    |  See below                                                            |
 
 	"""),
 dcc.Markdown("""
@@ -23,46 +23,27 @@ dcc.Markdown("""
 	This is a table with two rows and two columns that reports the number of false positives, false negatives, true positives, and true negatives.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')],
-	style={'text-align':'center'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')], style={'text-align':'center'}),
 dcc.Markdown("""
 	#### ROC AUC
 	The ROC curve is created by plotting the true positive rate (y-axis) and the false positive rate (x-axis) at various threshold settings.
 	It illustrates the diagnostic ability of the model as its discrimination threshold is varied. The optimal value of the AUC a model is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true', style={"width": "80%"})], style={"text-align":"center"}),
 dcc.Markdown("""
 	#### Precision Recall AUC
 	A precision-recall curve is a plot of the precision (y-axis) and the recall (x-axis) for different probability thresholds.
 	The optimal value of the PR AUC is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true', style={"width": "80%"})], style={"text-align":"center"}),
 dcc.Markdown("""
 	#### Kaplan-Meier survival curve
 	This curve estimates the survival of the patients included in the study. High-risk patients are patients that the model identifies as having a high risk of dying within 10 years from diagnosis.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true', style={"width": "80%"})], style={"text-align":"center"}),
 dcc.Markdown("""
 	#### The 20 most important features predicting survival
 	The SHAP value is a way to estimate the contribution of a feature to the prediction performed by the model.
@@ -72,11 +53,5 @@ dcc.Markdown("""
 	- Correlation: Younger patients (blue dots) at diagnosis have a lower risk of dyng within 10 years (negative SHAP values).
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-])
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true', style={"width": "80%"})], style={"text-align":"center"}),
 ]
--- tabs/explain.py
+++ tabs/explain.py
@@ -13,36 +13,20 @@ layout = [
 	Men in the screening group were offered annual PSA testing for 6 years and digital rectal examination for 4 years. 
 	The subjects and health care providers received the results and decided on the type of follow-up evaluation. 
 	Results of the trial were published in three articles:"""),
-	
-	dcc.Link('Andriole GL et al., New England Journal of Medicine, 2009', 
-		 href='https://www.ncbi.nlm.nih.gov/pubmed/19297565',
-		 style={'font-weight': 'bold', 'color': '#820000'}),
-	
+	html.A("Andriole GL et al., New England Journal of Medicine, 2009", href='https://www.ncbi.nlm.nih.gov/pubmed/19297565', target="_blank", style={'font-weight': 'bold', 'color': '#820000', 'text-decoration': 'none'}),
 	html.P([]),
-	
-	dcc.Link('Andriole GL et al., Journal of the National Cancer Institute, 2012', 
-		 href='https://www.ncbi.nlm.nih.gov/pubmed/22228146',
-		 style={'font-weight': 'bold', 'color': '#820000'}),
-	
+	html.A("Andriole GL et al., Journal of the National Cancer Institute, 2012", href='https://www.ncbi.nlm.nih.gov/pubmed/22228146', target="_blank", style={'font-weight': 'bold', 'color': '#820000', 'text-decoration': 'none'}),
 	html.P([]),
-	
-	dcc.Link('Pinsky PF et al., Cancer, 2017', 
-		 href='https://www.ncbi.nlm.nih.gov/pubmed/19297565',
-		 style={'font-weight': 'bold', 'color': '#820000'}),
-	
-	html.P([]),
-	
+	html.A("Pinsky PF et al., Cancer, 2017", href='https://www.ncbi.nlm.nih.gov/pubmed/19297565', target="_blank", style={'font-weight': 'bold', 'color': '#820000', 'text-decoration': 'none'}),
+	html.P([html.Br()]),
 	dcc.Markdown("""
 	#### Data
 	The data with a 13-years median follow-up was obtained from the National Cancer Institute Cancer Data Access System:"""),
-	
-	dcc.Link('NCI CDAS', 
-		 href='https://cdas.cancer.gov/plco/',
-		 style={'font-weight': 'bold', 'color': '#820000'}),
-	
+	html.A("National Cancer Institute Cancer Data Access System", href='https://cdas.cancer.gov/plco/', target="_blank", style={'font-weight': 'bold', 'color': '#820000', 'text-decoration': 'none'}),
+	html.P([html.Br()]),
 	dcc.Markdown("""
 	#### Model Creation
 	To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=13,738). 
-	The dataset was split into a training (n=10,990) and a testing (n=2748) dataset.
+	The dataset was split into a training (n=10,990) and a testing (n=2,748) dataset.
 	XGBoost was used to predict 10-year overall survival. Hyperparameters were tuned with Bayesian Optimization 
 	in a nested cross-validation manner. Performances were evaluated on the test dataset.""")]
