cad2aa88d9937a182cda538b0bd619158733244f
Jean-Emmanuel <jean-emmanuel@DN52ekj3.SUNet>
2020-01-21 14:36:51 -0800

    First commit

--- tabs/evaluate.py
+++ tabs/evaluate.py
@@ -9,13 +9,13 @@ dcc.Markdown("""
 	### Metrics
 	Evaluating models trained on unbalanced classes (the two classes we need to predict are not present in the dataset at the same ratio) can be difficult. We provide several methods to assess the performances of the model. All metrics are reported on the test dataset.
 
-	|  Metric      |  Result   |  Definition					                   |
+	|  Metric      |  Result   |  Definition					                                       |
 	|--------------|-----------|-----------------------------------------------------------------------|
-	| Accuracy     | 91.90%    |  Number of correct predictions / total number of input samples        |
-	| Precision    | 80.78%    |  Number of correct positive predictions / number of positive samples  |
-	| Recall       | 77.50%    |  Number of correct positive results / number of all positive samples  |
-	| ROC AUC      | 92.04%    |  See below                                                            |
-	| PR AUC       | 84.88%    |  See below                                                            |
+	| Accuracy     | 91.34%    |  Number of correct predictions / total number of input samples        |
+	| Precision    | 80.70%    |  Number of correct positive predictions / number of positive samples  |
+	| Recall       | 78.51%    |  Number of correct positive results / number of all positive samples  |
+	| ROC AUC      | 93.04%    |  See below                                                            |
+	| PR AUC       | 86.83%    |  See below                                                            |
 
 	"""),
 dcc.Markdown("""
@@ -23,60 +23,35 @@ dcc.Markdown("""
 	This is a table with two rows and two columns that reports the number of false positives, false negatives, true positives, and true negatives.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')],
-	style={'text-align':'center'}),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSConfusionMatrix.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### ROC AUC
 	The ROC curve is created by plotting the true positive rate (y-axis) and the false positive rate (x-axis) at various threshold settings.
-	It illustrates the diagnostic ability of the model as its discrimination threshold is varied. The optimal value of the AUC a model is 1.
+	It illustrates the diagnostic ability of the model as its discrimination threshold is varied. The optimal value of the AUC is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSROC.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### Precision Recall AUC
 	A precision-recall curve is a plot of the precision (y-axis) and the recall (x-axis) for different probability thresholds.
 	The optimal value of the PR AUC is 1.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSPrecisionRecall.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### Kaplan-Meier survival curve
 	This curve estimates the survival of the patients included in the study. High-risk patients are patients that the model identifies as having a high risk of dying within 10 years from diagnosis.
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-]),
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOS.png?raw=true')], style={'text-align' : 'center'}),
 dcc.Markdown("""
 	#### The 20 most important features predicting survival
 	The SHAP value is a way to estimate the contribution of a feature to the prediction performed by the model.
 	- Feature importance: Variables are ranked in descending order.
-	- Impact: The horizontal location shows whether the effect of that value is associated with a higher or lower prediction.
+	- Impact: The horizontal location shows whether the feature is associated with a higher or lower risk of dying.
 	- Original value: Color shows whether that variable is high (in red) or low (in blue) for that observation.
-	- Correlation: Younger patients (blue dots) at diagnosis have a lower risk of dyng within 10 years (negative SHAP values).
+	- Correlation: As an example, in this model younger patients (blue dots) at diagnosis have a lower risk of dyng within 10 years (negative SHAP values).
 	"""),
 html.Div([
-	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true',
-		 style={
-			 "width": "100%",
-			 "text-align":"center"
-		 }
-		)
-])
+	html.Img(src='https://github.com/jebibault/ProstateCancerSurvival/blob/master/figures/10yOSTreeExplainer.png?raw=true')], style={'text-align' : 'center'})
 ]
--- tabs/intro.py
+++ tabs/intro.py
@@ -4,23 +4,28 @@ import dash_html_components as html
 
 from app import app
 
-layout = html.Div(
-	html.P([
-		html.Br(),
-		'The management of prostate cancer that is detected on the basis of prostate-specific antigen (PSA) levels remains controversial. In the United States alone, each year, an estimated 180,890 new cases will be diagnosed and 26,120 men will die from the disease. PSA testing has resulted in a significant increase in the diagnosis and treatment of prostate cancer, but many men do not benefit from intervention because the disease is either indolent or disseminated at diagnosis. Prostate cancer often progresses slowly, and many men die of competing causes.', 
-		html.Br(),
-		html.Br(),
-		html.Br(),
-		'In order to assess whether a patient with prostate cancer could actually benefit from cancer treatment, and not die from another cause, we created a model to predict 10-year overall survival. This model was trained on data from the prospective randomized multicenter trial PLCO that assessed the interest of prostate cancer screening with annual PSA testing for 6 years and digital rectal examination for 4 years.',
-		html.Br(),
-		html.Br(),
-		html.Br(),
-		'In this trial, 76,693 men at 10 U.S. study centers were randomly assigned to receive either annual screening (38,343 subjects) or usual care as the control (38,350 subjects).',
-		html.Br(),
-		html.Br(),
-		html.Br(),
-		'To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=13,738).'
-	])
-)
+layout = [
+dcc.Markdown("""
+#### What is this model for?"""),
+html.P([
+	'The management of prostate cancer that is detected on the basis of prostate-specific antigen (PSA) levels remains controversial. In the United States alone, each year, an estimated 180,890 new cases will be diagnosed and 26,120 men will die from the disease. PSA testing has resulted in a significant increase in the diagnosis and treatment of prostate cancer.',
+	html.Br(),
+	html.Br(),
+	'Many men do not benefit from treatment because the disease is either indolent or disseminated at diagnosis. Because prostate cancer progresses slowly, patients often die of competing causes.', 
+	html.Br(),
+	html.Br(),
+	'In order to assess whether a patient with prostate cancer could actually benefit from cancer treatment, and not die from another cause, we created a model to predict 10-year overall survival. This model was trained on data from the prospective randomized multicenter trial PLCO that assessed the interest of prostate cancer screening with annual PSA testing for 6 years and digital rectal examination for 4 years. In this trial, 76,693 men at 10 U.S. study centers were randomly assigned to receive either annual screening (n=38,343) or usual care as the control (n=38,350).',
+	html.Br(),
+	html.Br(),
+	'To build this model we selected the patients that were diagnosed with prostate cancer during follow-up (n=8,776).',
+	html.Br(),
+	html.Br(),
+]),
+dcc.Markdown("""
+#### Disclaimer:
+All information contained on this application is for general informational purposes only and does not constitute medical or other professional advice. It should not be used for medical purpose. The information you find on this application is provided as is and to the extent permitted by law, the creators disclaim all other warranties of any kind, whether express, implied, statutory or otherwise. In no event will the authors or its affiliates be liable for any damages whatsoever, including without limitation, direct, indirect, incidental, special, exemplary, punitive or consequential damages, arising out of the use, inability to use or the results of use of this application, any websites linked to this application or the materials or information or services contained at any or all such websites, whether based on warranty, contract, tort or any other legal theory and whether or not advised of the possibility of such damages. If your use of the materials, information or services from this application or any website linked to this application results in the need for servicing, repair or correction of equipment or data, you assume all costs thereof.'""")
+]
+
+
 
 
