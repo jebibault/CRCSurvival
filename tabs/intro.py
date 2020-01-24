568027d2db6a978778713e57894b3b0c2075bbee
Jean-Emmanuel <jean-emmanuel@MacBook-Pro-16-de-Jean-Emmanuel.local>
2020-01-23 21:09:16 -0800

    CSS model added

--- tabs/intro.py
+++ tabs/intro.py
@@ -63,6 +63,7 @@ dcc.Markdown("""
 	html.Br(),
 dcc.Markdown("""
 ### Disclaimer:
+This application does not store any of the data provided for the predictions.
 All information contained on this application is for general informational purposes only and does not constitute medical or other professional advice. It should not be used for medical purpose. The information you find on this application is provided as is and to the extent permitted by law, the creators disclaim all other warranties of any kind, whether express, implied, statutory or otherwise. In no event will the authors or its affiliates be liable for any damages whatsoever, including without limitation, direct, indirect, incidental, special, exemplary, punitive or consequential damages, arising out of the use, inability to use or the results of use of this application, any websites linked to this application or the materials or information or services contained at any or all such websites, whether based on warranty, contract, tort or any other legal theory and whether or not advised of the possibility of such damages. If your use of the materials, information or services from this application or any website linked to this application results in the need for servicing, repair or correction of equipment or data, you assume all costs thereof.""")
 ]
 
