4bd1bc2c9a6c5737195580d84404576dc3ee0098
Jean-Emmanuel <jean-emmanuel@DNab421786.stanford.edu>
2020-03-24 14:43:03 -0700

    Bug fix

--- assets/gtag.js
+++ assets/gtag.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-157472354-1');
+  gtag('config', 'UA-161770539-1');
